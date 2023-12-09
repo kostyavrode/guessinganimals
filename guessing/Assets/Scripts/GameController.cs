b363f6c410291fc916276c1294796d3bc9fe3483
kostyavrode <inbloomf3@gmail.com>
2023-12-09 13:52:27 +0300

    df
    dtgh

--- guessing/Assets/Scripts/GameController.cs
+++ guessing/Assets/Scripts/GameController.cs
@@ -11,6 +11,7 @@ public class GameController : MonoBehaviour
     public Transform animalSpawnPoint;
     public Transform startCameraPosition;
     public Transform guessCameraPosition;
+    public GameObject startPanel;
     public GameObject winPanel;
     public GameObject losePanel;
     public GameObject answerPanel;
@@ -20,10 +21,30 @@ public class GameController : MonoBehaviour
     private int money;
     private int guessedAnimals;
     private GameObject currentAnimal;
+    public ParticleSystem fireworks;
+    public UniWebView uniWebView;
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+    private void Start()
+    {
+        PanelErasing(startPanel);
+        CheckCoins();
+        if (PlayerPrefs.HasKey("money"))
+        {
+            money = PlayerPrefs.GetInt("money");
+        }
+    }
     public void StartGame()
     {
         SpawnAnimal();
         StartCameraMotion();
+    }
+    private void PanelErasing(GameObject panel)
+    {
+        panel.transform.localScale = Vector3.zero;
+        panel.transform.DOScale(Vector3.one, 1f);
     }
     private void StartCameraMotion()
     {
@@ -46,7 +67,7 @@ public class GameController : MonoBehaviour
         answerPanel.SetActive(true);
         bool tempbool = false;
          guessTexts = answerPanel.GetComponentsInChildren<TMP_Text>();
-        for (int i = 0; i < guessTexts.Length; i++)
+        for (int i = 0; i < guessTexts.Length-1; i++)
         {
             guessTexts[i].text = animals[Random.Range(0, animals.Count)].animalType;
             if (guessTexts[i].text==currentAnimalType)
@@ -56,8 +77,9 @@ public class GameController : MonoBehaviour
         }
         if (!tempbool)
         {
-            guessTexts[1].text = currentAnimalType;
+            guessTexts[Random.Range(0,4)].text = currentAnimalType;
         }
+        guessTexts[4].text = "Who is this?";
     }
     public void GuessAnimalType(int a)
     {
@@ -77,14 +99,14 @@ public class GameController : MonoBehaviour
         guessedAnimals += 1;
         if ((guessedAnimals == 10))
         {
-
-
             Debug.Log("Right");
             money += 10;
             PlayerPrefs.SetInt("money", money);
             PlayerPrefs.Save();
             winPanel.SetActive(true);
+            PanelErasing(winPanel);
             answerPanel.SetActive(false);
+            CheckCoins();
         }
         else
         {
@@ -98,9 +120,47 @@ public class GameController : MonoBehaviour
         Debug.Log("Wrong");
         answerPanel.SetActive(false);
         losePanel.SetActive(true);
+        PanelErasing(losePanel);
+    }
+    private void CheckCoins()
+    {
+        if (PlayerPrefs.HasKey("money"))
+        {
+            moneyBar.text = PlayerPrefs.GetInt("money").ToString();
+        }
+        else
+        {
+            moneyBar.text = "0";
+        }
     }
     public void RestartGame()
     {
+        winPanel.SetActive(false);
+        losePanel.SetActive(false);
+        camera.transform.DORotate(new Vector3(17.74f, 116.815f, 0), 1f);
+        camera.transform.DOMove(startCameraPosition.position, 1f).OnComplete(ReloadScene);
+    }
+    public void PlayFireworks()
+    {
+        if (PlayerPrefs.GetInt("money")>=10)
+        {
+            PlayerPrefs.SetInt("money", PlayerPrefs.GetInt("money") - 10);
+            PlayerPrefs.Save();
+            CheckCoins();
+            fireworks.Play();
+        }
+    }
+    private void ReloadScene()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void OpenWebview(string url)
+    {
+        var webviewObject = new GameObject("UniWebview");
+        uniWebView = webviewObject.AddComponent<UniWebView>();
+        uniWebView.Frame = new Rect(0, 0, Screen.width, Screen.height);
+        uniWebView.SetShowToolbar(true, false, true, true);
+        uniWebView.Load(url);
+        uniWebView.Show();
     }
 }
